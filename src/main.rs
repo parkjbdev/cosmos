5f7ca87d4cb5764a1de269477870189cf8327e18
parkjbdev <dev@parkjb.com>
2023-09-21 15:30:20 +0900

    A Freestanding Rust Binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+// Panic handler
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// Linker looks for a function named '_start' by default
+// Name-Mangling is disabled to prevent the linker from renaming the function
+#[no_mangle]
+// "C" calling convention is used to prevent the compiler from adding the Rust ABI
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
