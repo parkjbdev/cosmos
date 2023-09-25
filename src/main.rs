c65ae092f55bb9cb991f5e5a2e15029bd6b05c19
parkjbdev <dev@parkjb.com>
2023-09-25 20:06:48 +0900

    A Minimal Rust Kernel

--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,14 @@ fn panic(_info: &PanicInfo) -> ! {
 #[no_mangle]
 // "C" calling convention is used to prevent the compiler from adding the Rust ABI
 pub extern "C" fn _start() -> ! {
+    static HELLO: &[u8] = b"Hello World!";
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
