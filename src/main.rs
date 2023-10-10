e4cc3083621c65c6cbaa5fb6b005b45b1b8965b2
parkjbdev <dev@parkjb.com>
2023-10-11 02:08:09 +0900

    VGA Text Mode: Simple Print

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,14 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+mod vga_buffer;
+
 // Linker looks for a function named '_start' by default
 // Name-Mangling is disabled to prevent the linker from renaming the function
 #[no_mangle]
 // "C" calling convention is used to prevent the compiler from adding the Rust ABI
 pub extern "C" fn _start() -> ! {
-    static HELLO: &[u8] = b"Hello World!";
-    let vga_buffer = 0xb8000 as *mut u8;
+    vga_buffer::print_hello();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
     loop {}
 }
