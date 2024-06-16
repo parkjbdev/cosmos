fb1a9675a673773f1e81cd653fed72cd9dbb2794
Park JongBeum <dev@parkjb.com>
2024-06-16 12:01:47 +0900

    build: sptr package for building from mac

--- src/arch/aarch64/dtb.rs
+++ src/arch/aarch64/dtb.rs
@@ -2,5 +2,5 @@ use super::constants::*;
 use hermit_dtb::Dtb;
 
 pub fn get_dtb<'a>() -> Dtb<'a> {
-    unsafe { Dtb::from_raw(core::ptr::from_exposed_addr(DEVICE_TREE as usize)).unwrap() }
+    unsafe { Dtb::from_raw(sptr::from_exposed_addr(DEVICE_TREE as usize)).unwrap() }
 }
