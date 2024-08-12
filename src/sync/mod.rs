7a0a547c0a766b050728504f649615ba95a5d2e4
Park JongBeum <dev@parkjb.com>
2024-08-12 15:43:22 +0900

    feat: lock

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -1,3 +1,15 @@
 pub mod spinlock;
-// pub mod null_lock;
+pub mod null_lock;
 
+pub mod interface {
+    pub trait Mutex {
+        type Data;
+        fn lock<'a, R>(&'a self, f: impl FnOnce(&'a mut Self::Data) -> R) -> R;
+    }
+
+    pub trait ReadWriteEx {
+        type Data;
+        fn read<'a, R>(&'a self, f: impl FnOnce(&'a Self::Data) -> R) -> R;
+        fn write<'a, R>(&'a self, f: impl FnOnce(&'a mut Self::Data) -> R) -> R;
+    }
+}
--- src/sync/null_lock.rs
+++ src/sync/null_lock.rs
@@ -1,8 +1,7 @@
-use crate::arch::exception::exec_with_irq_disabled;
+use crate::arch::exception::irq;
 use core::cell::UnsafeCell;
 
-// use super::interface;
-
+// Simple Null Lock
 pub struct NullLock<T>
 where
     T: ?Sized,
@@ -22,7 +21,39 @@ impl<T> NullLock<T> {
     }
 }
 
-impl<T> interface::Mutex for NullLock<T> {
+impl<T> super::interface::Mutex for NullLock<T> {
+    type Data = T;
+
+    fn lock<'a, R>(&'a self, f: impl FnOnce(&'a mut Self::Data) -> R) -> R {
+        // In a real lock, there would be code encapsulating this line that ensures that this
+        // mutable reference will ever only be given out once at a time.
+        let data = unsafe { &mut *self.data.get() };
+
+        f(data)
+    }
+}
+
+// IRQ Safe Null Lock
+pub struct IRQSafeNullLock<T>
+where
+    T: ?Sized,
+{
+    data: UnsafeCell<T>,
+}
+
+unsafe impl<T> Send for IRQSafeNullLock<T> where T: ?Sized + Send {}
+unsafe impl<T> Sync for IRQSafeNullLock<T> where T: ?Sized + Send {}
+
+impl<T> IRQSafeNullLock<T> {
+    /// Create an instance.
+    pub const fn new(data: T) -> Self {
+        Self {
+            data: UnsafeCell::new(data),
+        }
+    }
+}
+
+impl<T> super::interface::Mutex for IRQSafeNullLock<T> {
     type Data = T;
 
     fn lock<'a, R>(&'a self, f: impl FnOnce(&'a mut Self::Data) -> R) -> R {
@@ -31,7 +62,7 @@ impl<T> interface::Mutex for NullLock<T> {
         let data = unsafe { &mut *self.data.get() };
 
         // TODO: Implement IRQ Safe lock
-        exec_with_irq_disabled(|| f(data))
+        irq::exec_with_irq_disabled(|| f(data))
     }
 }
 
